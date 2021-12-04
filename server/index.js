4ca5a54c23749b9487eaa6b7f0195bfd2acab285
9karat <vlad49391@gmail.com>
2021-12-05 00:39:42 +0200

    serGAY pipiska_malenkaya

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT ||  5000
-const authRouter = require('./authRouter')
+const authRouter = require('./routes/authRouter')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use('/auth', authRouter)
+app.use(cors())
 
 const start = async () => {
   try {
